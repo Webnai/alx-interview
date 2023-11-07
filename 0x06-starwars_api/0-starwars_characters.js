4eb107ac0d6ec175d571bf7d09e1e11ad0d40ec4
webnai <wilbertboadzo144@gmail.com>
2023-11-07 20:35:41 +0000

    start wars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,48 +1,29 @@
 #!/usr/bin/node
-
-// require - Imports a module
 const request = require('request');
-// import request from 'request';
-// getMovieCharacters - Prints all characters of a Star Wars movie
-function getMovieCharacters (movieId) {
-  const filmUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
-  request(filmUrl, (error, response, filmData) => {
-    if (!error && response.statusCode === 200) {
-      const film = JSON.parse(filmData);
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-      const characterUrls = film.characters;
-
-      const characterPromises = characterUrls.map((characterUrl) => {
-        return new Promise((resolve, reject) => {
-          request(characterUrl, (error, response, characterData) => {
-            if (!error && response.statusCode === 200) {
-              const character = JSON.parse(characterData);
-              resolve(character.name);
-            } else {
-              resolve(
-                `Failed to fetch character data for URL: ${characterUrl}`
-              );
-            }
-          });
-        });
+request(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  }
+  const characters = JSON.parse(body).characters;
+  const promises = characters.map((character) => {
+    return new Promise((resolve, reject) => {
+      request(character, (err, res, body) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(JSON.parse(body).name);
       });
+    });
+  }
+  );
+  Promise.all(promises).then((names) => {
+    names.forEach((name) => {
+      console.log(name);
+    });
+  }
+  );
+});
 
-      Promise.all(characterPromises).then((characterNames) => {
-        characterNames.forEach((characterName) => console.log(characterName));
-      });
-    } else {
-      console.error(`Failed to fetch film data for ID: ${movieId}`);
-    }
-  });
-}
-
-const args = process.argv.slice(2);
-
-if (args.length !== 1) {
-  console.log('Usage: star_wars_characters_request.js <movie_id>');
-  process.exit(1);
-}
-
-const movieId = args[0];
-getMovieCharacters(movieId);
